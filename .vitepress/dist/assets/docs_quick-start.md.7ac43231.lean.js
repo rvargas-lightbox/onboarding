565fd71aa4dd7d7ae5bc4cadf3a46360f2e6e842
Ricardo Vargas <im@ricardovargas.me>
2023-03-31 08:15:52 -0400

    Removing unnecessary files.

--- .vitepress/dist/assets/docs_quick-start.md.7ac43231.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,U as a}from"./chunks/framework.3eb667f5.js";const m=JSON.parse('{"title":"Welcome Message","description":"","frontmatter":{},"headers":[],"relativePath":"docs/quick-start.md"}'),i={name:"docs/quick-start.md"},r=a("",14),n=[r];function l(s,c,u,d,h,p){return o(),t("div",null,n)}const f=e(i,[["render",l]]);export{m as __pageData,f as default};
